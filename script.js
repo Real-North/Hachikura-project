1dd393ad25bead90e0e76fbb87b2b59e203c034c
Makita <j2148@J2148noMacBook-Pro.local>
2021-01-04 13:25:27 +0900

    script change

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 $(function() {
     $('.headerTitle').hide().slideDown(500);
     $('.searchingBar').hide().slideDown(800);
-    $('.list_box').hide();
+    $('.list_box').hide().fadeIn(2000);
 
 });
-$(function() {
-    $('#search').click(function() {
-        $('.list_box').slideDown();
-    });
-});
+// $(function() {
+//     $('#search').click(function() {
+//         $('.list_box').slideDown();
+//     });
+// });
